8d632aeff920674b765f4f154c37590e932d65c1
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-09-21 16:46:43 -0400

    Foreach test.

---
+++ test/test_foreach.cpp
@@ -0,0 +1,85 @@
+/**
+ * Template C++ file.
+ * Function with references to pointers: func(int *&ptr);
+ */
+/********************* Header Files ***********************/
+/* C++ Headers */
+#include <iostream> /* Input/output objects. */
+//#include <fstream> /* File operations. */
+#include <sstream> /* String stream. */
+#include <string> /* C++ String class. */
+//#include <new> /* Defines bad_malloc exception, new functions. */
+//#include <typeinfo> /* Casting header. */
+//#include <exception> /* Top level exception header. */
+//#include <stdexcept> /* Derived exception classes. */
+
+/* STL Headers */
+//#include <vector>
+//#include <list>
+//#include <deque>
+//#include <stack>
+//#include <queue>
+//#include <priority_queue>
+//#include <bitset>
+//#include <set> // multiset for multiple keys allowed.
+//#include <map> // multimap for multiple keys allowed.
+//#include <utility> // Has pair for map.
+//#include <algorithm>
+//#include <numeric>
+//#include <functional> // Functional objects.
+//#include <iterator> // Contains back_inserter function and like.
+
+/* C Headers */
+//#include <cstdlib>
+//#include <cstddef>
+//#include <cctype>
+//#include <cstring>
+//#include <cstdio>
+//#include <climits>
+//#include <cassert>
+
+/* Project Headers */
+#include <boost/foreach.hpp>
+#include <gtest/gtest.h>
+
+/******************* Constants/Macros *********************/
+
+
+/**************** Namespace Declarations ******************/
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+/******************* Type Definitions *********************/
+/* For enums: Try to namesapce the common elements.
+ * typedef enum {
+ *	VAL_,
+ * } name_e;
+ */
+
+/* For structs:
+ * typedef struct name_s {
+ *	int index;
+ * } name_t;
+ */
+
+/****************** Class Definitions *********************/
+
+
+/****************** Global Functions **********************/
+
+TEST(BoostEx, BoostForeachSimple) {
+    string hello("Hello");
+    std::stringstream ss;
+
+    BOOST_FOREACH(char c, hello) {
+        ss << c << ",";
+    }
+    EXPECT_STREQ("H,e,l,l,o,", ss.str().c_str());
+}
+
+/* Notes:
+ * Force call to use another version of virtual function: baseP->Item_base::net_price(42);
+ *
+ */

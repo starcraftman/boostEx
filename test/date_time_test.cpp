976401d2f0958b33e066bc6f25b145e5f171e61f
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-09-24 22:44:37 -0400

    Update taghighlight & better namespace vars.

--- test/date_time_test.cpp
+++ test/date_time_test.cpp
@@ -51,9 +51,9 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-namespace d  = ::boost::gregorian;
-namespace pt = ::boost::posix_time;
-namespace lt = ::boost::local_time;
+namespace greg  = ::boost::gregorian;
+namespace pTime = ::boost::posix_time;
+namespace lTime = ::boost::local_time;
 
 /******************* Type Definitions *********************/
 /* For enums: Try to namesapce the common elements.
@@ -83,25 +83,25 @@ void datesEqual(const T &expect, const T &actual) {
 
 TEST(BoostDateGregorian, SimpleCreate) {
     std::stringstream ss;
-    d::date date1(2002, d::Feb, 1);
+    greg::date date1(2002, greg::Feb, 1);
 
     ss << date1;
     ASSERT_STREQ("2002-Feb-01", ss.str().c_str());
 }
 
 TEST(BoostDateGregorian, Arithmetic) {
-    d::date dateStart(2002, d::Feb, 1), dateExpect(2004, d::Feb, 3);
-    d::date dateActual = dateStart + d::years(2) + d::days(2);
+    greg::date dateStart(2002, greg::Feb, 1), dateExpect(2004, greg::Feb, 3);
+    greg::date dateActual = dateStart + greg::years(2) + greg::days(2);
 
-    datesEqual<d::date>(dateExpect, dateActual);
+    datesEqual<greg::date>(dateExpect, dateActual);
 }
 
 TEST(BoostDateGregorian, DaysIterator) {
-    std::vector<d::date> vExpect = {d::date(2002, d::Feb, 1),
-	d::date(2002, d::Feb, 2), d::date(2002, d::Feb, 3),
-	d::date(2002, d::Feb, 4), d::date(2002, d::Feb, 5),
+    std::vector<d::date> vExpect = {d::date(2002, greg::Feb, 1),
+	greg::date(2002, greg::Feb, 2), greg::date(2002, greg::Feb, 3),
+	greg::date(2002, greg::Feb, 4), greg::date(2002, greg::Feb, 5),
     };
-    d::date dateStart(2002, d::Feb, 1), dateEnd(2002, d::Feb, 6);
+    greg::date dateStart(2002, greg::Feb, 1), dateEnd(2002, greg::Feb, 6);
 
     /* Iterate the days from a start to end */
     std::vector<d::date>::const_iterator itE = vExpect.begin();
@@ -111,28 +111,28 @@ TEST(BoostDateGregorian, DaysIterator) {
 }
 
 TEST(BoostDateGregorian, DateGenerator) {
-    d::date dateStart(2002, d::Feb, 1);
-    d::date dateExpect(2002, d::Feb, 7);
-    d::date dateActual = d::next_weekday(dateStart, d::greg_weekday(d::Thursday));
+    greg::date dateStart(2002, greg::Feb, 1);
+    greg::date dateExpect(2002, greg::Feb, 7);
+    greg::date dateActual = greg::next_weekday(dateStart, greg::greg_weekday(greg::Thursday));
 
-    datesEqual<d::date>(dateExpect, dateActual);
+    datesEqual<greg::date>(dateExpect, dateActual);
 }
 
 TEST(BoostDateGregorian, DateNextDay) {
-    d::date dateExpect(2004, d::Sep, 6);
-    typedef d::nth_day_of_the_week_in_month nth_dow;
-    nth_dow laborDay(nth_dow::first, d::Monday, d::Sep);
+    greg::date dateExpect(2004, greg::Sep, 6);
+    typedef greg::nth_day_of_the_week_in_month nth_dow;
+    nth_dow laborDay(nth_dow::first, greg::Monday, greg::Sep);
 
-    datesEqual<d::date>(dateExpect, laborDay.get_date(2004));
+    datesEqual<greg::date>(dateExpect, laborDay.get_date(2004));
 }
 
 TEST(BoostDatePosixTime, SimpleCreate) {
-    d::date dateStart(2002, d::Feb, 1);
-    pt::ptime timeActual(dateStart, pt::hours(5));
+    greg::date dateStart(2002, greg::Feb, 1);
+    pTime::ptime timeActual(dateStart, pTime::hours(5));
     string expect("2002-Feb-01 05:00:00");
-    pt::ptime timeExpect(pt::time_from_string(expect));
+    pTime::ptime timeExpect(pTime::time_from_string(expect));
 
-    datesEqual<pt::ptime>(timeExpect, timeActual);
+    datesEqual<pTime::ptime>(timeExpect, timeActual);
 }
 
 TEST(BoostDatePosixTime, InputCreate) {
@@ -140,24 +140,24 @@ TEST(BoostDatePosixTime, InputCreate) {
     std::stringstream ss;
     ss << expect;
 
-    pt::ptime timeExpect(pt::time_from_string(expect));
-    pt::ptime timeActual;
+    pTime::ptime timeExpect(pTime::time_from_string(expect));
+    pTime::ptime timeActual;
     ss >> timeActual;
 
-    datesEqual<pt::ptime>(timeExpect, timeActual);
+    datesEqual<pTime::ptime>(timeExpect, timeActual);
 }
 
 TEST(BoostDateTimeZone, SimpleConversion) {
-    lt::tz_database tzDb;
+    lTime::tz_database tzDb;
     tzDb.load_from_file(
 	"libs/share/boost/date_time/date_time_zonespec.csv");
-    lt::time_zone_ptr nycTz = tzDb.time_zone_from_region("America/New_York");
-    lt::time_zone_ptr phxTz(new lt::posix_time_zone("MST-07:00:00"));
+    lTime::time_zone_ptr nycTz = tzDb.time_zone_from_region("America/New_York");
+    lTime::time_zone_ptr phxTz(new lTime::posix_time_zone("MST-07:00:00"));
 
-    lt::local_date_time phxDeparture(d::date(2005, d::Apr, 2), pt::hours(23),
-	phxTz, lt::local_date_time::NOT_DATE_TIME_ON_ERROR);
-    pt::time_duration flight_length = pt::hours(4) + pt::minutes(30);
-    lt::local_date_time phxArrival = phxDeparture + flight_length;
+    lTime::local_date_time phxDeparture(greg::date(2005, greg::Apr, 2), pTime::hours(23),
+	phxTz, lTime::local_date_time::NOT_DATE_TIME_ON_ERROR);
+    pTime::time_duration flight_length = pTime::hours(4) + pTime::minutes(30);
+    lTime::local_date_time phxArrival = phxDeparture + flight_length;
 
     string tExpectDepartPHX("2005-Apr-02 23:00:00 MST");
     string tExpectArrivalNYC("2005-Apr-03 06:30:00 EDT");

55ca413d818eea7ef1b1fe8d7b04fa4c90c986fb
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2014-10-01 09:53:41 -0400

    Simple mutex example

--- test/threads_mutex_test.cpp
+++ test/threads_mutex_test.cpp
@@ -12,6 +12,7 @@
 //#include <typeinfo> /* Casting header. */
 //#include <exception> /* Top level exception header. */
 //#include <stdexcept> /* Derived exception classes. */
+#include <random>
 
 /* STL Headers */
 #include <vector>
@@ -35,7 +36,7 @@
 //#include <cctype>
 //#include <cstring>
 //#include <cstdio>
-//#include <climits>
+#include <climits>
 //#include <cassert>
 
 /* Project Headers */
@@ -45,7 +46,9 @@
 #include <gtest/gtest.h>
 
 /******************* Constants/Macros *********************/
-
+#define MAX_SLEEP_TIME_ms 500
+std::default_random_engine gGenerator;
+std::uniform_int_distribution<long> gDistribution(0, LONG_MAX);
 
 /**************** Namespace Declarations ******************/
 using std::cin;
@@ -54,21 +57,113 @@ using std::endl;
 using std::string;
 
 /******************* Type Definitions *********************/
-/* For enums: Try to namesapce the common elements.
- * typedef enum {
- *	VAL_,
- * } name_e;
- */
-
-/* For structs:
- * typedef struct name_s {
- *	int index;
- * } name_t;
- */
+typedef std::vector<long> queue_t;
 
 /****************** Class Definitions *********************/
+long randLong(long max = 0) {
+    long val = gDistribution(gGenerator);
+    if (max) {
+        val = val % max;
+    }
+
+    return val;
+}
+
+class Worker {
+public:
+    Worker() : threadRunning(false) { };
+    virtual ~Worker() {};
+
+    virtual void execute() = 0;
+    virtual void start() = 0;
+    void stop() { threadRunning = false; }
+    void join() { thread.join(); }
+    bool isRunning() { return threadRunning; }
+
+protected:
+    bool threadRunning;
+    boost::thread thread;
+    /* Static to be same accross all instances. */
+    static queue_t work;
+    static boost::mutex workMutex;
+};
+boost::mutex Worker::workMutex;
+queue_t Worker::work;
+
+class ProducerTryLock : public Worker {
+public:
+    virtual void start() {
+        threadRunning = true;
+        this->thread = boost::thread(&ProducerTryLock::execute, this);
+    }
+    virtual void execute() {
+        while (threadRunning) {
+            if (workMutex.try_lock()) {
+                long val = randLong();
+                work.push_back(val);
+
+                printf("^^^ %ld\n", val);
+
+                boost::posix_time::milliseconds delay(50);
+                boost::this_thread::sleep(delay);
+                workMutex.unlock();
+            } else {
+                printf("^== \n");
+            }
+
+            // Slow things down a little
+            boost::posix_time::milliseconds delayTime(randLong() % MAX_SLEEP_TIME_ms);
+            boost::this_thread::sleep(delayTime);
+        }
+    }
+};
+
+class ConsumerTryLock : public Worker {
+public:
+    virtual void start() {
+        threadRunning = true;
+        this->thread = boost::thread(&ConsumerTryLock::execute, this);
+    }
+    virtual void execute() {
+        while (threadRunning) {
+            if (workMutex.try_lock()) {
+                if (work.size()) {
+                    long val = work.back();
+                    work.pop_back();
+
+                    printf("vvv %ld\n", val);
+                }
+
+                boost::posix_time::milliseconds delay(50);
+                boost::this_thread::sleep(delay);
+                workMutex.unlock();
+            } else {
+                printf("v==\n");
+            }
+
+            // Slow things down a little
+            boost::posix_time::milliseconds delayTime(randLong() % MAX_SLEEP_TIME_ms);
+            boost::this_thread::sleep(delayTime);
+        }
+    }
+};
 
 /****************** Global Functions **********************/
+TEST(BoostThread, AcquireMutex) {
+    boost::mutex m;
+    ProducerTryLock t1;
+    ConsumerTryLock t2;
+    t1.start();
+    t2.start();
+
+    boost::posix_time::milliseconds workTime(2000);
+    boost::this_thread::sleep(workTime);
+
+    t1.stop();
+    t2.stop();
+    t1.join();
+    t2.join();
+}
 
 /* Notes:
  * Force call to use another version of virtual function: baseP->Item_base::net_price(42);
